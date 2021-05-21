20ce28321ff1938bd9cd8ec0ee8cdd5814923102
platipus25 <40722163+platipus25@users.noreply.github.com>
2021-05-21 13:48:19 -0700

    Parse time

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 use structopt::StructOpt;
+use time::Time;
 
 /// Search for a pattern in a file and display the lines that contain it.
-/*#[derive(StructOpt)]
+#[derive(StructOpt)]
 struct Cli {
     /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
-}*/
+    #[structopt(parse(try_from_str = parse_time))]
+    time: Time,
+    ///// The path to the file to read
+    //#[structopt(parse(from_os_str))]
+    //path: std::path::PathBuf,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let args = Cli::from_args();
-    let string = "01:05";
-    let date = parse_time(string)?;
-    println!("Hello, world! {:?}", date);
+    let args = Cli::from_args();
+    println!("Hello, world! {}", args.time.format("%r"));
+
+    let now = time::OffsetDateTime::try_now_local();
+    println!("{:?}", now);
+
+    /*let new_time = now.with_time(args.time);
+
+
+    println!("{:?}", new_time);*/
     Ok(())
 }
 
-fn parse_time(s: &str) -> Result<time::Time, time::ParseError> {
-    match time::Time::parse(s, "%-H:%-M") {
-        Ok(date) => { Ok(date) },
-        Err(err) => { Ok(time::Time::parse(s, "%-H")?) },
-    }
+fn parse_time(s: &str) -> Result<Time, time::ParseError> {
+    Time::parse(s, "%-I:%-M%p")
+        .or_else(|_e| Time::parse(s, "%-H:%-M"))
+        .or_else(|_e| Time::parse(s, "%-H"))
 }
