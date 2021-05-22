2e8348106c0a537b3f625803f9f85df977597780
platipus25 <40722163+platipus25@users.noreply.github.com>
2021-05-21 17:14:52 -0700

    Make it work

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
 use structopt::StructOpt;
-use time::Time;
+use chrono::prelude::*;
+use hhmmss::Hhmmss;
 
-/// Search for a pattern in a file and display the lines that contain it.
+/// Take a time and print the time remaining until that time. If `time` has already passed today,
+/// then count to `time` tomorrow.
 #[derive(StructOpt)]
 struct Cli {
     /// The pattern to look for
     #[structopt(parse(try_from_str = parse_time))]
-    time: Time,
-    ///// The path to the file to read
-    //#[structopt(parse(from_os_str))]
-    //path: std::path::PathBuf,
+    time: NaiveTime,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let args = Cli::from_args();
-    println!("Hello, world! {}", args.time.format("%r"));
 
-    let now = time::OffsetDateTime::try_now_local();
-    println!("{:?}", now);
+    let mut date = Local::today();
 
-    /*let new_time = now.with_time(args.time);
+    if args.time < Local::now().time() {
+        date = date.succ();
+    }
 
+    let date = date.and_time(args.time).unwrap();
+    let time_left = date - Local::now();
 
-    println!("{:?}", new_time);*/
-    Ok(())
+    let relative_day = if date.date() == Local::today() { "today" } else { "tomorrow" };
+    println!("{} until {} {}", time_left.hhmmss(), args.time.format("%r"), relative_day);
 }
 
-fn parse_time(s: &str) -> Result<Time, time::ParseError> {
-    Time::parse(s, "%-I:%-M%p")
-        .or_else(|_e| Time::parse(s, "%-H:%-M"))
-        .or_else(|_e| Time::parse(s, "%-H"))
+/// Parse a time from a string.
+/// First look for 12 hour format (%-I:%-M%p)
+/// Then for 24-hour format  ("%-H:%-M")
+/// Then for just an hour (%-H)
+fn parse_time(s: &str) -> Result<NaiveTime, chrono::ParseError> {
+    NaiveTime::parse_from_str(s, "%-I:%-M%p")
+        .or_else(|_e| NaiveTime::parse_from_str(s, "%-H:%-M"))
+        .or_else(|e| match s.parse::<u32>() {
+            Ok(hour) => Ok(NaiveTime::from_hms(hour, 0, 0)),
+            Err(_err) => Err(e)
+        })
 }
