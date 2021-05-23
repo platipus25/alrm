8200ddb77790ef15d70da6cda177c57a51ea90e1
platipus25 <40722163+platipus25@users.noreply.github.com>
2021-05-22 18:41:21 -0700

    Change main loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use structopt::StructOpt;
 use chrono::prelude::*;
 use hhmmss::Hhmmss;
-use console::Term;
+use console::{ Term, Style };
 use std::thread;
 
 /// Take a time and print the time remaining until that time. If `time` has already passed today,
@@ -27,18 +27,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     }
 
     let date = date.and_time(args.time).unwrap();
-    let time_left = date - Local::now();
 
-    let relative_day = if date.date() == Local::today() { "today" } else { "tomorrow" };
-    if args.repeat {
-        println!("Time left until {} {}", args.time.format("%r"), relative_day);
-        while date > Local::now() {
-            term.write_line(&(date - Local::now()).hhmmssxxx())?;
-            thread::sleep(std::time::Duration::from_millis(10));
-            term.clear_last_lines(1)?;
-        }
-    } else {
-        println!("{} until {} {}", time_left.hhmmss(), args.time.format("%r"), relative_day);
+    let yellow = Style::new().bright().yellow();
+    loop {
+        let time_left = date - Local::now();
+        let relative_day = if date.date() == Local::today() { "today" } else { "tomorrow" };
+        let output = format!("{} until {} {}", yellow.apply_to(time_left.hhmmss()), args.time.format("%-I:%M%P"), relative_day);
+        term.write_line(&output)?;
+
+        if !args.repeat || date < Local::now() { break }
+
+        thread::sleep(std::time::Duration::from_millis(1000));
+        term.clear_last_lines(1)?;
     }
     Ok(())
 }
@@ -51,7 +51,10 @@ fn parse_time(s: &str) -> Result<NaiveTime, chrono::ParseError> {
     NaiveTime::parse_from_str(s, "%-I:%M%p")
         .or_else(|_e| NaiveTime::parse_from_str(s, "%-H:%M"))
         .or_else(|e| match s.parse::<u32>() {
-            Ok(hour) => Ok(NaiveTime::from_hms(hour, 0, 0)),
+            Ok(hour) => match NaiveTime::from_hms_opt(hour, 0, 0) {
+                Some(time) => Ok(time),
+                None => Err(e)
+            },
             Err(_err) => Err(e)
         })
 }
